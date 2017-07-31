bd772ed64c6196d36d4b6b70ac7627e96aab7120
salimm <salimm@cs.pitt.edu>
2017-07-31 11:33:05 -0400

    missing required commit:
    1. removed uneccessary files
    2. added functions to DataUtils for in[][] String[][] and also enabled
    double to accept range

--- src/me/salimm/sfb/DataUtils.java
+++ src/me/salimm/sfb/DataUtils.java
@@ -1,15 +1,53 @@
 package me.salimm.sfb;
 
+import java.util.Random;
+
 public class DataUtils {
 
-	
-	public static  double[][] generateNumericData(int width, int length){
+	public static double[][] generateNumericData(int width, int length, double minRange, double maxRange) {
+		double span = maxRange - minRange;
 		double[][] data = new double[width][length];
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < length; j++) {
-				data[i][j] = Math.random();
+				data[i][j] = Math.random() * span + minRange;
 			}
 		}
 		return data;
+	}
+
+	public static int[][] generateIntegerData(int width, int length, int minRange, int maxRange) {
+		double span = maxRange - minRange;
+		int[][] data = new int[width][length];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < length; j++) {
+				data[i][j] = (int) (Math.random() * span + minRange);
+			}
+		}
+		return data;
+	}
+
+	public static String[][] generateStringData(int width, int length, int minRange, int maxRange) {
+		int span = maxRange - minRange;
+		String[][] data = new String[width][length];
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < length; j++) {
+				int len = (int) (Math.random() * span + minRange);
+				data[i][j] = getRandomString(len);
+			}
+		}
+		return data;
+	}
+
+	public static String getRandomString(int length) {
+		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 - ";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		while (salt.length() < length) { // length of the random string.
+			int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+			salt.append(SALTCHARS.charAt(index));
+		}
+		String saltStr = salt.toString();
+		return saltStr;
+
 	}
 }
--- src/me/salimm/sfb/Test.java
+++
@@ -1,7 +0,0 @@
-package me.salimm.sfb;
-
-public class Test {
-	public static void main(String[] args) {
-		DataUtils.generateNumericData(100, 10000000);
-	}
-}
